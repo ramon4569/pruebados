ff913df8ba27fbc9649fba33d615ed06be1a8beb
Alienwere <Alienwere@DESKTOP-7C5S4IR>
2025-05-09 18:12:42 -0400

    Agregar archivos de proyecto.

---
+++ pruebados/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace capturanombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su nombre");
+        }
+    }
+}
+
+public class Class1
+{
+    public Class1()
+    {
+    }
+}
